16aec22b8fb29bc66d65d242615aa4ce20e73f68
christopher garcia <71852007+christopher-garcia1@users.noreply.github.com>
2025-08-08 12:40:43 +0700

    forgot to make it loop

--- 4.JAVASCRIPT-SECTION/Auto+Typing/Auto Typing/Starter/script.js
+++ 4.JAVASCRIPT-SECTION/Auto+Typing/Auto Typing/Starter/script.js
@@ -1,6 +1,7 @@
 const typed = new Typed('.auto-type',
     {
         strings: ['Developer.', 'Gamer.', 'Musician.'],
-        typeSpeed: 50
+        typeSpeed: 50,
+        loop: true
         
     });
